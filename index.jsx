c2edc69541044a3f5a229b72ff6f81010d9f504f
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-31 13:41:57 +0700

    Bootstrap the Host pages

--- index.jsx
+++ index.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from './pages/Vans'
-import VanDetail from './pages/VanDetail'
+import Vans from './pages/Vans/Vans'
+import VanDetail from './pages/Vans/VanDetail'
+import Layout from './components/Layout'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
 
 
 import './server'
@@ -12,18 +16,17 @@ import './server'
 function App() {
     return (
       <BrowserRouter>
-      <header>
-        <Link className='site-logo' to='/'>#VANLIFE</Link>
-        <nav>
-          <Link to='/about'>About</Link>
-          <Link to='/vans'>Vans</Link>
-        </nav>
-      </header>
+      
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/vans" element={<Vans />}/>
-          <Route path="/vans/:id" element={<VanDetail />}/>
+          <Route element={<Layout />} >
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/vans" element={<Vans />}/>
+            <Route path="/vans/:id" element={<VanDetail />}/>
+            <Route path="/host" element={<Dashboard />}/>
+            <Route path="/host/income" element={<Income />}/>
+            <Route path="/host/reviews" element={<Reviews />}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     )
