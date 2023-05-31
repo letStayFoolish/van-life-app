6fcb26847b9b87c918ebd5ed4740400f613e83ab
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-31 16:53:17 +0700

    Nested routes done

--- index.jsx
+++ index.jsx
@@ -8,8 +8,13 @@ import VanDetail from './pages/Vans/VanDetail'
 import Layout from './components/Layout'
 import HostLayout from './components/HostLayout';
 import Dashboard from './pages/Host/Dashboard'
+import HostVans from './pages/Host/HostVans'
+import HostVanDetail from './pages/Host/HostVanDetail'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
+import HostVanInfo from './pages/Host/HostVanInfo'
+import HostVanPricing from './pages/Host/HostVanPricing'
+import HostVanPhotos from './pages/Host/HostVanPhotos'
 
 
 import './server'
@@ -29,9 +34,16 @@ function App() {
               <Route index element={<Dashboard />}/>
               <Route path="income" element={<Income />}/>
               <Route path="reviews" element={<Reviews />}/>
+              <Route path='vans' element={<HostVans />} />
+              <Route path='vans/:id' element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path='pricing' element={<HostVanPricing />} />
+                <Route path='photos' element={<HostVanPhotos />} />
+              </Route>
             </Route>
           </Route>
         </Routes>
+
       </BrowserRouter>
     )
   }
---
+++ pages/Host/HostVanDetail.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useParams, Link, Outlet, NavLink } from 'react-router-dom'
+
+export default function HostVanDetail() {
+
+  const { id } = useParams()
+
+  const [currentVan, setCurrentVan] = React.useState(null)
+
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616"
+  }
+
+  React.useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+    .then(res => res.json())
+    .then(data => setCurrentVan(data.vans))
+  }, [])
+
+  if(currentVan) {
+      return (
+        <section>
+          <Link
+              to=".."
+              relative='path'
+              className="back-button"
+          >
+            &larr; <span>Back to all vans</span>
+          </Link>
+          <div className="host-van-detail-layout-container">
+            <div className="host-van-detail">
+                <img src={currentVan.imageUrl} />
+                <div className="host-van-detail-info-text">
+                    <i
+                        className={`van-type van-type-${currentVan.type}`}
+                    >
+                        {currentVan.type}
+                    </i>
+                    <h3>{currentVan.name}</h3>
+                    <h4>${currentVan.price}/day</h4>
+                </div>
+            </div>
+            <nav className="host-van-detail-nav">
+              <NavLink style={({isActive}) => isActive ? activeStyles : null} to='.' end >Details</NavLink>
+              <NavLink style={({isActive}) => isActive ? activeStyles : null} to='pricing' >Pricing</NavLink>
+              <NavLink style={({isActive}) => isActive ? activeStyles : null} to='photos' >Photo</NavLink>
+            </nav>
+          <Outlet context={{currentVan}} />
+          </div>
+        </section>
+      )
+    } else {
+      return <h1>Loading...</h1>
+    }
+}
+
