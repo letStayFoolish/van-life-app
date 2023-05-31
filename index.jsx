1dcb471d78aa275e4311c75898aa58b89286b7cb
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-31 12:55:38 +0700

    Add navbar, pages: Home, About, Vans as well as VanDetail.

--- index.jsx
+++ index.jsx
@@ -3,19 +3,27 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from './pages/Vans'
+import VanDetail from './pages/VanDetail'
+
+
+import './server'
 
 function App() {
     return (
       <BrowserRouter>
       <header>
-        <Link to='/'>#VANLIFE</Link>
+        <Link className='site-logo' to='/'>#VANLIFE</Link>
         <nav>
           <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
         </nav>
       </header>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />}/>
+          <Route path="/vans/:id" element={<VanDetail />}/>
         </Routes>
       </BrowserRouter>
     )
